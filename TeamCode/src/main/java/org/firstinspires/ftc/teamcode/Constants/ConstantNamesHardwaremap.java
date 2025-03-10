c0787618d515fb7b82e4e86e9eea2decd49e5bb5
aesh09 <aviv.eshed09@gmail.com>
2025-03-10 11:26:11 +0200

    added hardwaremap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/ConstantNamesHardwaremap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/ConstantNamesHardwaremap.java
@@ -1,7 +1,20 @@
 package org.firstinspires.ftc.teamcode.Constants;
 
 public class ConstantNamesHardwaremap {
-    public static final String ARM = "arm";
-    public static final String ELEVATORLEFT = "liftMotorLeft"; //EX HUB 3
-    public static final String ELEVATORRIGHT = "liftMotorRight"; //CONT HUB 0
+
+    public static final String ARM = "arm"; //EX HUB 2
+    public static final String LOWGRIPPERRIRGHT = "loServoR"; // EX 3
+    public static final String LOWGRIPPERLEFT = "loServoL"; // EX 4
+    public static final String HIGHGRIPPER = "highGripperServo"; // EX 0
+    public static final String HORZ = "horz"; //CONT  3
+    public static final String SWEEPER = "sweeper"; //CONT  3
+    public static final String LIMITSWITCH = "limit_switch";
+    public static final String ELEVATORLEFT = "liftMotorLeft"; //EX  3
+    public static final String ELEVATORRIGHT = "liftMotorRight"; //CONT  0
+    public static final String WHEELFRONTLEFT = "leftFront"; //EX  1
+    public static final String WHEELFRONTRIGHT = "rightFront"; //CONT  1
+    public static final String WHEELBACKLEFT = "leftBack"; //EX  0
+    public static final String WHEELBACKRIGHT = "rightBack"; //CONT  2
+    public static final String GRIPPERSPINNERLEFT = "loRotL"; // EX 1
+    public static final String GRIPPERSPINNERRIGHT = "loRotR"; // EX 2
 }
